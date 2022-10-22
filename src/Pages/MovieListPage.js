3ee60a4e695e2bb905a1f9d6e066484dfb24efec
Thilanka Rodrigo <65930513+Thilanka0824@users.noreply.github.com>
2022-10-22 00:13:12 -0700

    built out Movie List Page

--- src/Pages/MovieListPage.js
+++ src/Pages/MovieListPage.js
@@ -1,7 +1,15 @@
-const MovieListPage = () => {
+
+import MovieCard from "../Components/MovieCard"
+
+const MovieListPage = (props) => {
     return (
         <div>
             <h1>Movie List Page</h1>
+            <div className="movie-list">
+                {props.movieList.map((movie)=>{
+                    return <MovieCard movie={movie}/>
+                })}
+            </div>
         </div>
     )
 }
